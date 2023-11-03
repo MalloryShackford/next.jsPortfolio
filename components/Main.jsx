1474dae590adecd6290530f34f894e236913f615
MShackford <93612680+MalloryShackford@users.noreply.github.com>
2023-11-03 15:50:19 -0400

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,12 +8,12 @@ const Main = () => {
     <div id='home' className='w-full h-screen text-center'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
-          <p className='uppercase text-sm tracking-widest text-gray-600'>Let's Build Something Together</p>
-          <h1 py-4 text-gray-700>Hi, I'm <span className='text-[#5651e5]'>Mallory</span></h1>
+          <p className='uppercase text-sm tracking-widest text-gray-600'>Let&apos;s Build Something Together</p>
+          <h1 py-4 text-gray-700>Hi, I&apos;m <span className='text-[#5651e5]'>Mallory</span></h1>
           <h1 py-2 text-gray-700>A Front-End Web Developer</h1> 
           <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
             I specialize in building exceptional digital experiences. 
-            Currently, I'm focused on building responsive front-end applications 
+            Currently, I&apos;m focused on building responsive front-end applications 
             while learning back-end technologies.
           </p>
           <div className='flex max-w-[330px] items-center justify-between m-auto py-4'>
