bd132fd31cca961ea8f5f1b57592aced9aca6bfa
MShackford <93612680+MalloryShackford@users.noreply.github.com>
2023-11-03 15:50:55 -0400

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -83,7 +83,7 @@ const Navbar = () => {
                     </div>
                 </div>
                 <div className='border-b border-gray-300 my-4'>
-                    <p className='w-[85%] md:w-[90%] py-4'>Let's build something together!</p>
+                    <p className='w-[85%] md:w-[90%] py-4'>Let&apos;s build something together!</p>
                 </div>
             </div>
             <div className='py-4 flex flex-col'>
@@ -105,7 +105,7 @@ const Navbar = () => {
                     </Link>
                 </ul>
                 <div className='pt-40'>
-                    <p className='uppercase tracking-widest text-[#5651e5]'>Let's Connect</p>
+                    <p className='uppercase tracking-widest text-[#5651e5]'>Let&apos;s Connect</p>
                     <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
                         <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
                         <a href="https://www.linkedin.com/in/mshackford/" target="_blank" rel="noopener"><FaLinkedinIn /></a>
