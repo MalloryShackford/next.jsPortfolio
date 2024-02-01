11dcb021fd3cc06c22ec5022d37851d8b91b5f96
MShackford <93612680+MalloryShackford@users.noreply.github.com>
2024-02-01 13:33:34 -0500

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -11,15 +11,16 @@ const About = () => {
             <h2 className='py-4'>Who I Am</h2>
             <p className='py-2 text-gray-600'>I am not your normal developer</p>
             <p className='py-2 text-gray-600'>
-              My experience spans over 15 years in the vibrant 
-              world of hospitality. I have mastered the art of service, attention to detail, 
-              and the nuanced art of customer satisfaction. Transitioning from orchestrating 
-              memorable guest experiences, I embraced the realm of education, spending a year 
-              imparting knowledge and nurturing minds as a teacher. My journey has now taken 
-              an exciting turn into the technological domain, where I am channeling my passion 
-              for learning and problem-solving into programming. This latest chapter is not 
-              just a career shift—it&apos;s a synthesis of my dedication to growth, my love for challenge, 
-              and my commitment to lifelong learning </p>
+            With over 15 years in the dynamic hospitality industry, 
+            I&apos;ve honed my skills in providing top-notch service, 
+            meticulous attention to detail, and ensuring customer satisfaction. 
+            Transitioning from creating memorable guest experiences, 
+            I ventured into education, dedicating a year to teaching and 
+            shaping young minds. Now, I&apos;ve embarked on a new chapter in technology, 
+            leveraging my passion for continuous learning and problem-solving in the world 
+            of programming. This career pivot reflects not just a change in profession, 
+            but my unwavering commitment to personal growth, embracing new challenges, 
+            and the pursuit of lifelong learning. </p>
             <a href="#projects"><p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects</p></a>
         </div>
         <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
